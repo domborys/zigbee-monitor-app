6169ea57b2e4c517a3f258109195779caa9a68a8
domborys <43849775+domborys@users.noreply.github.com>
2021-08-27 19:32:53 +0200

    Draft of front-end api layer

---
+++ webapp/zigbee-monitor/src/api.js
@@ -0,0 +1,47 @@
+export default {
+    getLayers,
+    getDiscoveryResults,
+    sendLayer,
+    deleteLayer,
+    makeMessageSocket
+};
+
+import axios from 'axios';
+
+async function getLayers(){
+    const response = await axios.get('/floors');
+    return response.data;
+}
+
+async function getDiscoveryResults(){
+    const response = await axios.get('/network-discovery');
+    return response.data;
+}
+
+async function sendLayer(layer, imageFile){
+    let returnedLayer;
+    if(typeof layer.id === 'number'){
+        returnedLayer = await axios.put('/floors/' + layer.id, layer);
+    }
+    else{
+        returnedLayer = await axios.post('/floors', layer);
+    }
+    if(imageFile){
+        let formData = new FormData();
+        formData.append('file', imageFile);
+        await axios.put('/floors/' + layer.id + '/image', formData, {
+            headers: {'Content-Type': 'multipart/form-data'},
+        })
+    }
+
+}
+
+async function deleteLayer(layer){
+    await axios.delete('/floors/' + layer.id);
+
+}
+
+function makeMessageSocket(){
+    const socket = new WebSocket('ws://localhost:8000/message-socket');
+    return socket;
+}

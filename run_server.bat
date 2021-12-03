8b5ccbe7f2686f1370696b99ca6437f72a4d04fd
domborys <43849775+domborys@users.noreply.github.com>
2021-12-03 13:12:45 +0100

    Added requirements file

--- run_server.bat
+++ run_server.bat
@@ -1,7 +1,10 @@
 @echo off
+call .\venv\Scripts\activate.bat
 pushd webserver
 start "" run_server.py
 popd
 pushd receiver
 start "" xbee_device_server.py
 popd
+call deactivate
+echo Everything started successfully.

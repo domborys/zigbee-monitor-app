2279f9ff9a7b37bf9801c2307dd3ad0f56b73a01
domborys <43849775+domborys@users.noreply.github.com>
2021-12-23 22:22:33 +0100

    Added reporting of potentiometer value

--- device/remote.py
+++ device/remote.py
@@ -1,16 +1,21 @@
 import xbee, time, machine
 
 LED_PIN_ID = "D4"
+POT_PIN_ID = "D2"
 TEMP_SEND_PERIOD = 20000
 
 def handle_packet(packet):
     payload = packet["payload"]
-    if payload.startswith(b"ledon"):
+    payload_split = payload.split()
+    command_name = payload_split[0]
+    if command_name == b"ledon":
         led_on()
-    elif payload.startswith(b"ledoff"):
+    elif command_name == b"ledoff":
         led_off()
-    elif payload.startswith(b"add"):
+    elif command_name == b"add":
         add_and_send_result(packet)
+    elif command_name ==  b"getpot":
+        send_pot_value(packet)
     print("Received {} from {}".format(packet["payload"], packet["sender_eui64"]))
 
 def led_on():
@@ -30,6 +35,13 @@ def add_numbers_in_payload(payload):
     payload_split = payload.split()
     return int(payload_split[1]) + int(payload_split[2])
 
+def send_pot_value(packet):
+    try:
+        value = pot_adc.read()
+        xbee.transmit(packet["sender_eui64"], "pot {}".format(value))
+    except Exception as err:
+        print(err)
+
 def send_temperature():
     temp = xbee.atcmd("TP")
     msg = "temp {}".format(temp)
@@ -39,6 +51,8 @@ def send_temperature():
         print(err)
 
 led_pin = machine.Pin(LED_PIN_ID, machine.Pin.OUT, value=1)
+pot_adc = machine.ADC(POT_PIN_ID)
+xbee.atcmd("AV", 2) # reference voltage VDD
 last_temp_time = time.ticks_ms()
 
 while True:

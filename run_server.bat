91d4fb0e8dea0884101beac70e521aa51992c214
domborys <43849775+domborys@users.noreply.github.com>
2022-01-18 01:19:52 +0100

    Changed logging defaults

--- run_server.bat
+++ run_server.bat
@@ -1,6 +1,6 @@
 @echo off
 call .\venv\Scripts\activate.bat
-start "" py -m webserver.run_server
-start "" py -m receiver.xbee_device_server
+start "Uvicorn" py -m webserver.run_server
+start "Coordinator handler" py -m receiver.xbee_device_server
 call deactivate
 echo Startup complete.
